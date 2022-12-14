8d104ff22f1df5446142458f14276794f00ccca1
Benjamin Rau <benjamin@thebuilders.de>
2022-12-14 22:59:57 +0100

    Fix copying properties files recursively

--- Omada-Controller/install.sh
+++ Omada-Controller/install.sh
@@ -179,7 +179,7 @@ do
 done
 
 # copy omada default properties for can be used when properties is mounted as volume
-cp -r properties/ "${OMADA_DIR}/properties.defaults"
+cp -r properties/* "${OMADA_DIR}/properties.defaults"
 
 # symlink for mongod
 ln -sf "$(command -v mongod)" "${OMADA_DIR}/bin/mongod"
